bee7fb573670c39c219ba4d0f3c2a4a913a7e01f
Detrick Lewis <Detricklew@gmail.com>
2023-04-16 08:36:24 -0500

    Implement various changes to all three files
    Various changes I should have commited over time. I will do better.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let equation = [];
 
+let state = [];
+
 let currentnum;
 
-let paranthesis = false;
+let paranthesiscount = 0;
 
 let num = false;
 
@@ -12,10 +14,20 @@ let funct = false;
 
 let perc = false;
 
+let division = false;
+
+let solutionCheck = false;
+function expo(x, f) {
+    return Number.parseFloat(x).toExponential(f);
+  }
 
 const numbers = document.querySelectorAll('.number');
 
 const functions = document.querySelectorAll('.func');
+
+const clear = document.querySelector('.clear');
+
+clear.addEventListener('click',clearDisplay);
 
 functions.forEach(funct =>{
     funct.addEventListener('click',loadFunction);
@@ -26,55 +38,232 @@ numbers.forEach(action =>{
 });
 
 function loadNumber(e){
-    if(funct){
-        equation.push(currentnum);
-        currentnum = null;
-        funct = false;
+    if(e.target.textContent == "="){
+        startOperation();
+        return;
+    }
+    if(perc || equation[equation.length - 1] == ")" && !currentnum) loadMultiplty();
+    if(e.target.textContent == "+/-"){
+        let check = numCheck();
+        if(check){
+            loadInput();
+            previewOperation();
+        }
+        return;
     }
     if(e.target.textContent == '.'){
+        if(solutionCheck){
+            stateChange('solution');
+        }
+        if(checkLength()) return;
         decimalCheck(e);
         loadInput();
+        previewOperation();
         return;
     }
     if(!currentnum){
+        console.log(`num is false`);
         currentnum = e.target.textContent;
-        num = true;
-        loadInput()
+        stateChange('number');
+        loadInput();
+        previewOperation();
+        return;
+    }
+    if(funct){
+        equation.push(currentnum);
+        currentnum = e.target.textContent;
+        stateChange('number')
+        loadInput();
+        previewOperation();
         return;
     }
     else{
+        if(solutionCheck){
+            stateChange('solution');
+            currentnum = e.target.textContent;
+            stateChange('number');
+            loadInput();
+            previewOperation();
+            return;
+        }
+        if(perc){
+            stateChange('number');
+        }
+        console.log('solutionchack');
+        if(checkLength()) return;
         currentnum = currentnum.toString() + e.target.textContent.toString();
-        loadInput()
+        loadInput();
+        previewOperation();
         return;
     }
 }
 
+function clearDisplay(){
+    equation = [];
+    currentnum = null
+    let input = document.querySelector('.input');
+    input.innerHTML = 0;
+    stateChange('clear');
+    previewOperation();
+    return;
+}
+
+function loadMultiplty(){
+    if(currentnum) equation.push(currentnum);
+    currentnum = "x";
+    stateChange('function');
+    return;
+}
+
+function previewOperation(){
+    let preview = [];
+    for(let i = 0; i < equation.length; i++){
+        preview.push(equation[i]);
+    }
+    let newnum = currentnum;
+    if (!funct && preview.length){
+        if(newnum) preview.push(newnum);
+        let solution = operate(preview);
+        solution = Math.round((solution + Number.EPSILON) * 100) / 100;
+        if(solution.toString().length > 15) solution = expo(solution, 15);
+        console.log(`hi solo ${solution} solution length ${solution.toString().length}`);
+        loadSolution(solution.toLocaleString("en-US"), ".solution");
+    }
+    else{
+        console.log("preview");
+        loadSolution('', ".solution");
+        return;
+    }
+}
+
+function startOperation(){
+    if (!funct && equation.length){
+        if(currentnum) equation.push(currentnum);
+        let solution = operate(equation);
+        solution = Math.round((solution + Number.EPSILON) * 100) / 100;
+        if(solution.toString().length > 15) solution = expo(solution, 15);
+        console.log(`hi solo ${solution} solution length ${solution.toString().length}`);
+        loadSolution(solution.toLocaleString("en-US").toUpperCase(), ".input");
+        loadSolution('', ".solution");
+        perc = false;
+        solutionCheck = true;
+        equation = [];
+        currentnum = solution;
+    }
+    else{
+        return;
+    }
+}
+
+function numCheck(){
+    if(currentnum) currentnum = currentnum.toString(); 
+    if(!num){
+        return false;
+    }
+    if(currentnum && num){
+        if(currentnum.includes('-')){
+            console.log('hey');
+             currentnum = currentnum.replace('-','');
+            return true;
+        }
+        else{
+            currentnum = '-' + currentnum;
+            return true;
+        }
+
+    }
+}
+
+function stateChange(state){
+    switch(state){
+        case 'solution':
+            num = false;
+            currentnum = null;
+            solutionCheck = !solutionCheck;
+            break;
+        case 'number':
+            num = true;
+            funct = false;
+            break;
+        case 'function':
+            solutionCheck = false;
+            decimal = false;
+            num = false;
+            funct = true;
+            perc = false;
+            break;
+        case 'clear':
+            decimal = false;
+            num = false;
+            funct = false;
+            perc = false;
+            solutionCheck = false;
+            paranthesiscount = 0;
+            break;
+    }
+}
+
 function percentageCheck(e){
-    if(num){
-        currentnum = currentnum.toString() + e.target.textContent.toString();
+    if(num && !perc){
+        equation.push((currentnum));
+        currentnum = e.target.textContent.toString();
         perc = true;
+        return;
+    }
+    else{
         return;
     }
 }
 
 function loadFunction(e){
     console.log('here func');
+    if(e.target.textContent == "( )"){
+        parenthesisLoader();
+        if(equation.length){
+            loadInput();
+            previewOperation();
+        } 
+        return;
+    }
     if(e.target.textContent == "%"){
         percentageCheck(e);
         loadInput();
+        previewOperation();
         return;
     }
     if (!funct && currentnum){
         equation.push(currentnum);
         currentnum = null;
         currentnum = e.target.textContent.toString();
-        funct = true;
-        loadInput()
+        if(e.target.textContent == "÷") {
+            division = true;
+        }
+        else{
+            division = false;
+        }
+        stateChange('function');
+        loadInput();
+        previewOperation();
+        return;
+    }
+    else if(!funct && equation[equation.length -1] == ')'){
+        currentnum = null;
+        currentnum = e.target.textContent.toString();
+        stateChange('function');
+        loadInput();
+        previewOperation();
         return;
     }
     else if(funct && (e.target.textContent != currentnum )){
+        if(e.target.textContent == "÷") {
+            division = true;
+        }
+        else{
+            division = false;
+        }
         currentnum = e.target.textContent.toString();
-        loadInput()
+        loadInput();
+        previewOperation();
         return;
     }
     else{
@@ -82,26 +271,161 @@ function loadFunction(e){
     }
 }
 
+function parenthesisLoader(){
+    if(!paranthesiscount && num) loadMultiplty();
+    if(funct && currentnum){
+        equation.push(currentnum);
+        currentnum = "(";
+        equation.push(currentnum);
+        currentnum = null;
+        console.log(equation);
+        paranthesiscount++;
+        return;
+    }
+    console.log(`paraKing para: ${paranthesiscount} num: ${num}`);
+    if (paranthesiscount && num){
+        console.log(`paraKing para: ${paranthesiscount} num: ${num}`);
+            if(currentnum) equation.push(currentnum);
+            currentnum = ")";
+            equation.push(currentnum);
+            currentnum = null;
+            paranthesiscount--;
+            return;
+    }
+
+
+}
+
+function loadSolution(value, destination){
+    let display = document.querySelector(destination);
+    display.innerHTML = value;
+    return;
+}
+
 function loadInput(){
     let input = document.querySelector('.input');
-    let display = equation.join("");
-    input.innerHTML = display + currentnum.toString();
+    let display = loadDisplay();
+    input.innerHTML = display;
+    return;
+}
+
+function checkLength(){
+    if(currentnum.toString().length == 15){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function checkParenthesis(){
+    let pare1;
+    let pare2;
+    let paracount = 0;
+    console.log(`equation length = ${equation.length}`);
+    for(let i = equation.length - 1; i >= 0; i--){
+        console.log(`current equation ${equation[i]} last equation ${equation[equation.length - 1]}`)
+        if (equation[equation.length - 1] != ')' || currentnum){
+            console.log("no para");
+            pare1 = null;
+            pare2 = null;
+            break;
+        }
+        if(paracount == 0 && equation[i].toString() == "("){
+            console.log('para accept');
+            pare1 = i;
+            pare2 = equation.length - 1;
+            break;
+        }
+        if(i != equation.length - 1 && equation[i] == ")"){
+            console.log("hi there");
+            paracount++;
+            continue;
+        }
+        if(paracount && equation[i] == "("){
+            paracount--;
+        }
+    }
+    console.log(`para1 = ${pare1} para2 = ${pare2}`);
+    if(!pare1 || !pare2){
+        return null;
+    }
+    else{
+        return [pare1,pare2];
+    }
+}
+
+function loadDisplay(){
+    let display = '';
+    let functcheck = false;
+    let pareload = checkParenthesis();
+    let functions = ['%','x','-','+','÷']
+    for (let i = 0; i < equation.length; i++){
+        if(pareload){
+            if (i == pareload[0] || i == pareload[1]){
+                console.log('check here');
+                display = display + `<span class ="function">${equation[i]}</span>`;
+                continue;
+            }
+        }
+        for(let j = 0; j < functions.length; j++){
+            if(equation[i] == functions[j]){
+                display = display + `<span class ="function">${equation[i]}</span>`;
+                functcheck = true;
+            }
+        }
+        if(!functcheck){
+            if(equation[i]== "(" || equation[i] == ")"){
+                display = display + equation[i];
+                continue;
+            }
+            let convert = Number(equation[i])
+            display = display + `<span>${convert.toLocaleString("en-US")}</span>`;
+        }
+        functcheck = false;
+    }
+    if(currentnum){
+        for (let i = 0; i < functions.length; i++){
+            if(currentnum == functions[i]){
+                display = display + `<span class ="function">${currentnum}</span>`;
+                functcheck = true;
+            }
+        }
+        if(!functcheck){
+            let convert = Number(currentnum);
+            display = display + `<span>${convert.toLocaleString("en-US")}</span>`;
+        }
+    }
+
+    return display;
 }
 
 function decimalCheck(e){
     if(decimal){
         return;
     }
-    else{
-        console.log('here');
+    else if(!decimal && funct){
+        equation.push(currentnum);
+        funct = !funct;
+        decimal = !decimal;
+        currentnum = 0 + e.target.textContent.toString();
+        return;
+    }
+    else if(!decimal && !num){
+        console.log(`here num`)
         decimal = !decimal; 
-        currentnum = currentnum.toString() + e.target.textContent.toString();
+        console.log(currentnum);
+        currentnum = 0 + e.target.textContent.toString();
+        console.log(currentnum);
+    }
+    else{
+        decimal = !decimal; 
+        currentnum = currentnum + e.target.textContent.toString();
     }
 }
 function operate(array){
     if(array.length == 1) return array[0];
     //executing PEMDAS
-    
+    console.log(array);
     //parenthesis
     let parantheis = search(array, ['(']);
     if(parantheis[0]){
@@ -133,23 +457,23 @@ function operate(array){
     console.log(percentag[0]);
 
     if(percentag[0]){
-        let value = array[percentag[1]].replace('%','');
+        let value = array[percentag[1]- 1];
 
         let percent = percentage(Number(value));
 
-        let newequation = arrayBuilder(array,percentag[1],percentag[1],percent);
+        let newequation = arrayBuilder(array,percentag[1]-1,percentag[1],percent);
         console.log(`parcentage array ${newequation}`);
         return operate(newequation);
     }
 
     //multiply/divide
 
-    let md = search(array,['X','÷']);
+    let md = search(array,['x','÷']);
     let pd;
 
     if(md[0]){
         switch(md[2]){
-            case 'X':
+            case 'x':
                 pd = multiply(Number(array[md[1]-1]),Number(array[md[1]+1]));
                 break;
             case '÷':
@@ -163,7 +487,6 @@ function operate(array){
 
     //Add/Subtract
     let as = search(array,['+','-']);
-    
     console.log(`add/subtract ${as}`);
 
     let sum;
@@ -178,7 +501,7 @@ function operate(array){
         }
 
         let newequation = arrayBuilder(array,as[1]-1,as[1]+1,sum);
-        console.log(`add/subtract new equation ${newequation}`)
+        console.log(`add currentnum.toString() + /subtract new equation ${newequation}`)
         return operate(newequation);
     }
 
@@ -194,12 +517,10 @@ function search(array, items){
             console.log(`${items[j]} = ${indexString}`);
             let test = indexString.includes(items[j]);
             console.log(test);
-            if(indexString.includes(items[j])){
+            if(indexString == items[j]){
             return [true, i, items[j]];
             }
         }
-        
-       
     }
 
     return [false];
@@ -207,10 +528,19 @@ function search(array, items){
 
 function parenthesisParser(array, start, stop){
 
-    if (start == stop){
-        return [array[start]];
-    }
     let newArray = [];
+
+    if (array[stop] != ")"){
+        for(let i = 0; i < array.length; i++){
+            if(i > start){
+                newArray.push(array[i]);
+            }
+            else{
+                continue;
+            }
+        }
+        return newArray;
+    }
 
     for(let i = 0; i < array.length; i++){
         if(i > start && i < stop){
@@ -244,27 +574,25 @@ function arrayBuilder(array, start, stop, newValue){
 }
 
 function parenthesisSearch(array, startIndex){
-    let paracheck = false;
-    if(array[startIndex].includes(')')){
-        return startIndex;
-    }
+    let paracount = 0;
+
 
     for(let i = startIndex+1; i < array.length; i++){
         let indexString = array[i].toString();
 
 
-        if(indexString.includes("(")){
+        if(indexString == "("){
             console.log(`here`)
-            paracheck = true;
+            paracount++;
         }
-        console.log(`check paracheck = ${paracheck}  index = ${indexString.includes(')')} indice = ${i} array length = ${array.length}`)
-        if(!paracheck && indexString.includes(')')){
+        console.log(`check paracheck = ${paracount}  index = ${indexString.includes(')')} indice = ${i} array length = ${array.length}`)
+        if(!paracount && indexString == ")"){
             return i;
         }
-        else if (paracheck && indexString.includes(')')){
-            paracheck = false;
+        if (paracount && indexString == ")"){
+            paracount--;
         }
-        else if(i == array.length - 1){
+        if(i == array.length - 1){
             return i;
         }
     }
